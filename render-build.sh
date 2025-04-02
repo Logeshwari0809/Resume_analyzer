eab05d8cf856de2c446cc43f129170e36a4cc75d
Logeshwari Ramesh <165378801+Logeshwari0809@users.noreply.github.com>
2025-04-02 20:22:10 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -4,4 +4,4 @@
 python -m spacy download en_core_web_sm
 
 # Start the FastAPI server using Gunicorn
-gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:10000
+gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:$PORT
