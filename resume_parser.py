bacb67ace58f127e7b97423a1be7080a6e1f4cd9
Logeshwari Ramesh <165378801+Logeshwari0809@users.noreply.github.com>
2025-04-03 23:30:57 +0530

    Update resume_parser.py

--- resume_parser.py
+++ resume_parser.py
@@ -1,7 +1,6 @@
 import PyPDF2
 
 def extract_text(file):
-    """Extracts text from uploaded PDF file."""
     pdf_reader = PyPDF2.PdfReader(file.file)
     text = "\n".join([page.extract_text() for page in pdf_reader.pages if page.extract_text()])
     return text
