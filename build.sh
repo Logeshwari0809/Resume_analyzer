acd0db07242c530e71f06d97f6fc405edc2ca671
Logeshwari Ramesh <165378801+Logeshwari0809@users.noreply.github.com>
2025-04-04 17:54:32 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Exit immediately if any command fails
-set -e
-
-echo "Creating virtual environment..."
-python -m venv venv
-source venv/bin/activate
-
 echo "Installing dependencies..."
 pip install --upgrade pip
 pip install -r requirements.txt
